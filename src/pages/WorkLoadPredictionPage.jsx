14caa3a2ddb17b80ebe7923964fa5d0de0c94eac
Maciej Rybak <maciek.rybak95@gmail.com>
2025-05-13 21:34:35 +0200

    Replace Calendar icon with BarChart2 and update heading text in WorkLoadPredictionPage

--- src/pages/WorkLoadPredictionPage.jsx
+++ src/pages/WorkLoadPredictionPage.jsx
@@ -1,16 +1,15 @@
-import { Calendar } from "lucide-react";
+import { BarChart2 } from "lucide-react";
 import WorkLoadPrediction from "../components/WorkLoadPrediction";
 
 const WorkLoadPredictionPage = () => {
   return (
     <div className=" mx-auto py-8 px-4 sm:px-6">
       <h2 className="text-2xl md:text-3xl font-bold mb-6 text-center text-gray-800 flex items-center justify-center gap-2">
-        <Calendar className="h-6 w-6 text-emerald-600" />
-       Grafik przewidywanego obciążenia
+        <BarChart2 className="h-6 w-6 text-emerald-600" />
+        Przewidywane obciążenia dla poszczególnych kolejek
       </h2>
 
       <WorkLoadPrediction queueName="Kolejka ogólna" queueId={1} />
-
     </div>
   );
 };
